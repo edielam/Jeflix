c6d7061184c91fb9c162ba1cb8801857da9172ea
edielam <edguard738@gmail.com>
2022-09-12 20:15:03 +0000

    adding feature selection

--- client/src/components/movlistitem/movlistitem.jsx
+++ client/src/components/movlistitem/movlistitem.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 
 export default function MovListItem({idx}){
     const [isHovered, setIsHovered] = useState(false)
+    const trailer = "";
     return (
         <div className='movlistitem'
             style={{left: isHovered && idx * 225 -50 + idx * 2.5}}
@@ -14,7 +15,29 @@ export default function MovListItem({idx}){
             src=''
             alt=''
             />
-
+            {isHovered && (
+        <>
+          <video src={trailer} autoPlay={true} loop />
+          <div className="itemInfo">
+            <div className="icons">
+              <PlayArrow className="icon" />
+              <Add className="icon" />
+              <ThumbUpAltOutlined className="icon" />
+              <ThumbDownOutlined className="icon" />
+            </div>
+            <div className="itemInfoTop">
+              <span>1 hour 14 mins</span>
+              <span className="limit">+16</span>
+              <span>1999</span>
+            </div>
+            <div className="desc">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Praesentium hic rem eveniet error possimus, neque ex doloribus.
+            </div>
+            <div className="genre">Action</div>
+          </div>
+        </>
+      )}
         </div>
     )
 }
