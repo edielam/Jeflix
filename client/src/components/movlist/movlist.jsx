1cf429320be057dbcdc7ca615ed6dfb89aa47095
edielam <edguard738@gmail.com>
2022-09-12 21:11:20 +0000

    adding feature selection

--- client/src/components/movlist/movlist.jsx
+++ client/src/components/movlist/movlist.jsx
@@ -1,9 +1,24 @@
 import './movlist.scss'
+import { ArrowBackIosOutline, ArrowForwardIosOutlined } from '@material-ui/icons'
+import MovListItem from "../movlistitem/movlistitem.scss"
+import { useState } from 'react'
 
-const movlist = () => {
+const MovList = () => {
+    const [isMoved, setIsMoved] = useState(false)
   return (
-    <div>movlist</div>
+    <div className='mList'>
+        <span className='mListTitle'>Continue to Watch</span>
+        <div className='wrapper'>
+            <ArrowBackIosOutline
+                className="slideArrow left"
+                style={{display: !isMoved && "none"}}
+            />
+            <div>
+                <MovListItem idx={0}/>
+            </div>
+        </div>
+    </div>
   )
 }
 
-export default movlist
+export default MovList;
