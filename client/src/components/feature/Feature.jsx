309cea7e9b945a81836551f826b2299348eb4d4e
edielam <edguard738@gmail.com>
2022-09-12 13:58:33 +0000

    adding feature selection

--- client/src/components/feature/Feature.jsx
+++ client/src/components/feature/Feature.jsx
@@ -1,3 +1,18 @@
 import { InfoOutlined, PlayArrow } from "@material-ui/icons";
 import './feature.scss';
 
+export default function Feature ({type}) {
+    return (
+        <div className="feature">
+            <img
+                src="https://www.hollywoodreporter.com/wp-content/uploads/2022/09/Sophia-Nomvete-Rings-of-Power-Still-Amazon-Publicity-H-2022.jpg?w=1296"
+                alt=""
+            />
+            <div className="fea-info">
+                <img
+                    src="https://assets-prd.ignimgs.com/2022/01/19/ringsofpowerlotr-sq-1642614059904.jpg"
+                    alt=""/>
+            </div>
+        </div>
+    )
+}
