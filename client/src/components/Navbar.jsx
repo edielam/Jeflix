a2d456ec774d43ea641e30ed4f981bb6fb00b36a
edielam <edguard738@gmail.com>
2022-09-12 11:12:01 +0000

    navbar component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,7 +2,21 @@ import './navbar.scss'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+        <div className='container'>
+            <div className='left'>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png" alt=''/>
+                <span>Homepage</span>
+                <span>Series</span>
+                <span>Movies</span>
+                <span>New and Popular</span>
+                <span>My List</span>
+            </div>
+            <div className='right'>
+
+            </div>
+        </div>
+    </div>
   )
 }
 
