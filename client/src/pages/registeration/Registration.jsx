68918a6e10abd74423d14cc7aa3fdb48adba32bf
edielam <edguard738@gmail.com>
2022-09-13 10:13:39 +0000

    adding remianing pages

--- client/src/pages/registeration/Registration.jsx
+++ client/src/pages/registeration/Registration.jsx
@@ -15,5 +15,18 @@ export default function Register() {
     const handleFinish = () => {
         setPassword(passwordRef.current.value);
   };
-    return ()
+    return (
+        <div className="register">
+      <div className="top">
+        <div className="wrapper">
+          <img
+            className="logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
+            alt=""
+          />
+          <button className="loginButton">Sign In</button>
+        </div>
+      </div>
+      </div>
+    )
 }
