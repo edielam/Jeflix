fd444f86856adae8edcddf2add48d28c1e057128
edielam <edguard738@gmail.com>
2022-09-13 10:16:37 +0000

    adding remianing pages

--- client/src/pages/registeration/Registration.jsx
+++ client/src/pages/registeration/Registration.jsx
@@ -16,17 +16,22 @@ export default function Register() {
         setPassword(passwordRef.current.value);
   };
     return (
-        <div className="register">
-      <div className="top">
-        <div className="wrapper">
-          <img
-            className="logo"
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
-            alt=""
-          />
-          <button className="loginButton">Sign In</button>
-        </div>
-      </div>
+        <div className="registeration">
+            <div className="reg-top">
+                <div className="reg-wrapper">
+                    <img className="logo" 
+                        src="https://raw.githubusercontent.com/edielam/ReactNodeJava/main/practice/JEFLIX2-removebg-preview.png" 
+                        alt=''
+                    />
+                <button className="loginButton">Sign In</button>
+                </div>
+            </div>
+            <div className="container">
+                <h1>Unlimited movies, TV shows, and more.</h1>
+                <h2>Watch anywhere. Cancel anytime.</h2>
+                <p>
+                Ready to watch? Enter your email to create or restart your membership.
+                </p>
       </div>
     )
 }
