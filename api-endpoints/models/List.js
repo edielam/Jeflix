df492c6c0984e538aa33c257fa5fbc4b052372fd
edielam <edguard738@gmail.com>
2022-09-13 18:17:00 +0000

    restructure

--- api-endpoints/models/List.js
+++ api-endpoints/models/List.js
@@ -1,7 +1,15 @@
 const mongoose = require("mongoose");
 
-const ListSkema = mongoose.Schema({
-    
-})
+const ListSkema = mongoose.Schema(
+    {
+        title: { type: String, required: true, unique: true },
+        type: { type: String },
+        genre: { type: String },
+        content:{type:Array}
+      },
+      { timestamps: true }
+);
+
+module.exports = mongoose.model("List", ListSkema);
 
 
