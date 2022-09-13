251bc83f1eef29c98504a1d44a0a4f2115c61b43
edielam <edguard738@gmail.com>
2022-09-13 08:00:24 +0000

    list items

--- client/src/components/movlistitem/movlistitem.jsx
+++ client/src/components/movlistitem/movlistitem.jsx
@@ -9,7 +9,7 @@ export default function ListItem({ index }) {
   return (
     <div
       className="movlistItem"
-      style={{ left: isHovered && index * 225 - 50 + index * 2.5 }}
+      //style={{ left: isHovered && index * 225 - 50 + index * 2.5 }}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
