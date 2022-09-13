38e4d34761cc502d6e076e1fc8e9e4cba2fefd20
edielam <edguard738@gmail.com>
2022-09-13 10:34:56 +0000

    adding remianing pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import "./app.scss"
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
+import Registeration from "./pages/registeration/Registration";
 
 const App = () => {
   return <>
     <Home/>
-    <Login/>;
+    <Login/>
+    <Registeration/>
   </>
 }
 export default App;
--- client/src/pages/registeration/Registration.jsx
+++ client/src/pages/registeration/Registration.jsx
@@ -2,50 +2,52 @@ import { useRef } from "react";
 import { useState } from "react";
 import "./registeration.scss";
 
-export default function Register() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+export default function Registeration() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const emailRef = useRef();
-    const passwordRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
 
-    const handleStart = () => {
-        setEmail(emailRef.current.value);
-    };
-    const handleFinish = () => {
-        setPassword(passwordRef.current.value);
+  const handleStart = () => {
+    setEmail(emailRef.current.value);
   };
-    return (
-        <div className="registeration">
-            <div className="reg-top">
-                <div className="reg-wrapper">
-                    <img className="logo" 
-                        src="https://raw.githubusercontent.com/edielam/ReactNodeJava/main/practice/JEFLIX2-removebg-preview.png" 
-                        alt=''
-                    />
-                <button className="log-btn">Sign In</button>
-                </div>
-            </div>
-            <div className="container">
-                <h1>Unlimited movies, TV shows, and more.</h1>
-                <h2>Watch anywhere. Cancel anytime.</h2>
-                <p>Ready to watch? Enter your email to create or restart your membership. </p>
-            {!email ? (
-                <div className="input">
-                    <input type="email" placeholder="Enter email address" ref={emailRef} />
-                    <button className="reg-btn" onClick={handleStart}>
-                    Get Started
-                    </button>
-                </div>
-                ) : (
-                <form className="input">
-                    <input type="password" placeholder="password" ref={passwordRef} />
-                    <button className="reg-btn" onClick={handleFinish}>
-                    Start
-                    </button>
-                </form>
-            )}
+  const handleFinish = () => {
+    setPassword(passwordRef.current.value);
+  };
+  return (
+    <div className="registeration">
+      <div className="top">
+        <div className="wrapper">
+        <img 
+            className="logo" 
+            src="https://raw.githubusercontent.com/edielam/ReactNodeJava/main/practice/JEFLIX2-removebg-preview.png" alt=''/>
+                
+          <button className="loginButton">Sign In</button>
         </div>
       </div>
-    )
+      <div className="container">
+        <h1>Unlimited movies, TV shows, and more.</h1>
+        <h2>Watch anywhere. Cancel anytime.</h2>
+        <p>
+          Ready to watch? Enter your email to create or restart your membership.
+        </p>
+        {!email ? (
+          <div className="input">
+            <input type="email" placeholder="email address" ref={emailRef} />
+            <button className="registerButton" onClick={handleStart}>
+              Get Started
+            </button>
+          </div>
+        ) : (
+          <form className="input">
+            <input type="password" placeholder="password" ref={passwordRef} />
+            <button className="registerButton" onClick={handleFinish}>
+              Start
+            </button>
+          </form>
+        )}
+      </div>
+    </div>
+  );
 }
