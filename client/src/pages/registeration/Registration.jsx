b3e8d0a4082e7112d0fddb46b137fb4c5c30201d
edielam <edguard738@gmail.com>
2022-09-13 10:20:09 +0000

    adding remianing pages

--- client/src/pages/registeration/Registration.jsx
+++ client/src/pages/registeration/Registration.jsx
@@ -30,6 +30,22 @@ export default function Register() {
                 <h1>Unlimited movies, TV shows, and more.</h1>
                 <h2>Watch anywhere. Cancel anytime.</h2>
                 <p>Ready to watch? Enter your email to create or restart your membership. </p>
+            {!email ? (
+                <div className="input">
+                    <input type="email" placeholder="Enter email address" ref={emailRef} />
+                    <button className="reg-btn" onClick={handleStart}>
+                    Get Started
+                    </button>
+                </div>
+                ) : (
+                <form className="input">
+                    <input type="password" placeholder="password" ref={passwordRef} />
+                    <button className="reg-btn" onClick={handleFinish}>
+                    Start
+                    </button>
+                </form>
+            )}
+        </div>
       </div>
     )
 }
