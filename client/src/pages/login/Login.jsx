1ca385f60a37b1bd11dc6970e41a248a976e2039
edielam <edguard738@gmail.com>
2022-09-13 09:03:23 +0000

    adding remianing pages

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -3,7 +3,18 @@ import "./login.scss"
 export default function Login() {
     return(
         <div className="login">
+            <div className="top">
+                <div className="wrapper">
 
+                </div>
+            </div>
+            <div className="container">
+                <form>
+                    <h1>Sign IN</h1>
+                    <input type="email" placeholder="Email or Phone number"/>
+                    <input type="password" placeholder="Password"/>
+                </form>
+            </div>
         </div>
     )    
 }
