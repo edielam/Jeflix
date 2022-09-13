2e504fbc1311cf18e6fb3ce501d31265a5e5affc
edielam <edguard738@gmail.com>
2022-09-13 18:09:45 +0000

    restructure

--- api-endpoints/index.js
+++ api-endpoints/index.js
@@ -9,6 +9,11 @@ mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
+    .then(() => console.log("DB Connection Successful"))
+    .catch((err) => {
+        console.error(err);
+    });
+
 app.listen(8800, () => {
     console.log("Backend service running")
 });
