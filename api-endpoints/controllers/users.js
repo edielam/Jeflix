3e7416ca3b636028761ca5dd575f2b774606419b
edielam <edguard738@gmail.com>
2022-09-14 22:43:32 +0000

    jwt auth

--- api-endpoints/controllers/users.js
+++ api-endpoints/controllers/users.js
@@ -76,7 +76,7 @@ router.get("/", verify, async (req, res) => {
 //GET USER STATS
 router.get("/stats", async (req, res) => {
   const today = new Date();
-  const lateYear = today.setFullYear(today.setFullYear() - 1);
+  const lastYear = today.setFullYear(today.setFullYear() - 1);
 
   try {
     const data = await User.aggregate([
