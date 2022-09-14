d9207aaf2a56a814598df8d93d78b5b686c9590b
edielam <edguard738@gmail.com>
2022-09-14 00:02:35 +0000

    restructure

--- api-endpoints/controllers/auth.js
+++ api-endpoints/controllers/auth.js
@@ -6,6 +6,8 @@ router.post("/register", async (req, res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: CryptoJS
+        password: CryptoJS.AES.encrypt(
+            req.body.password, process.env.SECRET_KEY
+        ).toString(),
     })
 })
