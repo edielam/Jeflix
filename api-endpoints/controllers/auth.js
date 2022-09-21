465fd7125a6d134c4ee4c57ea8314d4259734cb9
edielam <edguard738@gmail.com>
2022-09-21 09:56:30 +0000

    routing

--- api-endpoints/controllers/auth.js
+++ api-endpoints/controllers/auth.js
@@ -41,7 +41,7 @@ router.post("/login", async (req, res) => {
   
       res.status(200).json({ ...info, accessToken });
     } catch (err) {
-      res.status(500).json(err);
+      console.error(err);
     }
   });
   
