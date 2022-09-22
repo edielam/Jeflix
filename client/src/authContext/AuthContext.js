0d3f7d96e1d9d5a3c582aa6cc57af685aa11632a
edielam <edguard738@gmail.com>
2022-09-22 17:32:23 +0000

    fetching from api

---
+++ client/src/authContext/AuthContext.js
@@ -0,0 +1,8 @@
+import AuthReducer from "./AuthReducer";
+import { createContext, useEffect, useReducer } from "react";
+
+const INITIAL_STATE = {
+  user: JSON.parse(localStorage.getItem("user")) || null,
+  isFetching: false,
+  error: false,
+};
