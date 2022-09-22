ebf91e0516bc446aff3c77aab391e0b059a10f7e
edielam <edguard738@gmail.com>
2022-09-22 17:45:57 +0000

    fetching from api

--- client/src/pages/registeration/Registration.jsx
+++ client/src/pages/registeration/Registration.jsx
@@ -1,19 +1,30 @@
 import { useRef } from "react";
 import { useState } from "react";
+import { useNavigate } from "react-router";
 import "./registeration.scss";
+import axios from "axios";
 
 export default function Registeration() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const history = useNavigate();
 
   const emailRef = useRef();
   const passwordRef = useRef();
+  const usernameRef = useRef();
 
   const handleStart = () => {
     setEmail(emailRef.current.value);
   };
-  const handleFinish = () => {
+  const handleFinish = async (e) => {
+    e.preventDefault();
     setPassword(passwordRef.current.value);
+    setUsername(usernameRef.current.value);
+    try {
+      await axios.post("auth/register", { email,username, password });
+      history.push("/login");
+    } catch (err) {}
   };
   return (
     <div className="registeration">
@@ -41,6 +52,7 @@ export default function Registeration() {
           </div>
         ) : (
           <form className="input">
+            <input type="username" placeholder="username" ref={usernameRef} />
             <input type="password" placeholder="password" ref={passwordRef} />
             <button className="reg-btn" onClick={handleFinish}>
               Start
