55ff02ebd2c93832cedb9511b59f0459a09ce65e
edielam <edguard738@gmail.com>
2022-09-23 23:54:42 +0000

    login btn

--- client/src/pages/watch/Watch.jsx
+++ client/src/pages/watch/Watch.jsx
@@ -1,13 +1,18 @@
 import { ArrowBackOutlined } from "@material-ui/icons";
+import { useHistory } from "react-router";
 import "./watch.scss";
 
 export default function Watch() {
+  const history = useHistory();
+  const handleHome = () => {
+    history.push("/");
+  } 
   return (
     <div className="watch">
-      <div className="back">
-        <ArrowBackOutlined />
-        Home
-      </div>
+      <button className="back" onClick={() => {handleHome()}}>
+        <ArrowBackOutlined className="svg_icons" />
+        
+      </button>
       <iframe className="video" width="966" height="543" src="https://www.youtube.com/embed/x8UAUAuKNcU" 
       title="The Lord of the Rings: The Rings of Power - Official Trailer | Prime Video" frameBorder="0" 
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
