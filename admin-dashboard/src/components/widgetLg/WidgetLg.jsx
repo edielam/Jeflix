2e25f17f16c5a2fdd1e886c7a01c33ba9c513ad4
edielam <edguard738@gmail.com>
2022-09-28 03:19:14 +0000

    admin ui components

--- admin-dashboard/src/components/widgetLg/WidgetLg.jsx
+++ admin-dashboard/src/components/widgetLg/WidgetLg.jsx
@@ -8,6 +8,7 @@ export default function WidgetLg() {
     <div className="widgetLg">
       <h3 className="widgetLgTitle">Latest transactions</h3>
       <table className="widgetLgTable">
+        <tbody>
         <tr className="widgetLgTr">
           <th className="widgetLgTh">Customer</th>
           <th className="widgetLgTh">Date</th>
@@ -74,6 +75,7 @@ export default function WidgetLg() {
             <Button type="Approved" />
           </td>
         </tr>
+        </tbody>
       </table>
     </div>
   );
--- admin-dashboard/src/pages/home/Home.jsx
+++ admin-dashboard/src/pages/home/Home.jsx
@@ -1,10 +1,11 @@
 import Chart from "../../components/chart/Chart";
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
 import "./home.css";
-import { userData } from "../../dummyData";
+//import { userData } from "../../dummyData";
 import WidgetSm from "../../components/widgetSm/WidgetSm";
 import WidgetLg from "../../components/widgetLg/WidgetLg";
-import {useMemo, useState} from 'react';
+import {useMemo, useState, useEffect} from 'react';
+import axios from 'axios';
 
 export default function Home() {
   const MONTHS = useMemo(
@@ -25,10 +26,35 @@ export default function Home() {
     []
   );
   const [userStats, setUserStats] = useState([]);
+  useEffect(() => {
+    const getStats = async () => {
+      try {
+        const res = await axios.get("/users/stats", {
+          headers: {
+            token:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMmUzNzc4MzJlMTQwOTljODc1MzM4OSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2NDMzMjU3MywiZXhwIjoxNjY0NzY0NTczfQ.cNzTtcHO79ykzh87_M8JwPBL6bV1XABuLfrwGvTDG9Q",
+          },
+        });
+        const statsList = res.data.sort(function (a, b) {
+          return a._id - b._id;
+        });
+        statsList.map((item) =>
+          setUserStats((prev) => [
+            ...prev,
+            { name: MONTHS[item._id - 1], "New User": item.total},
+          ])
+        );
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getStats();
+  }, [MONTHS]);
+  console.log(userStats);
   return (
     <div className="home">
       <FeaturedInfo />
-      <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
+      <Chart data={userStats} title="User Analytics" grid dataKey="Active User"/>
       <div className="homeWidgets">
         <WidgetSm/>
         <WidgetLg/>
