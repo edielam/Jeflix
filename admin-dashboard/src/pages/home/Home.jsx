5a19f9f84c027b48d4d8c746f9640ea88346f707
edielam <edguard738@gmail.com>
2022-09-28 02:32:28 +0000

    admin ui components

--- admin-dashboard/src/pages/home/Home.jsx
+++ admin-dashboard/src/pages/home/Home.jsx
@@ -6,6 +6,26 @@ import WidgetSm from "../../components/widgetSm/WidgetSm";
 import WidgetLg from "../../components/widgetLg/WidgetLg";
 
 export default function Home() {
+  const MONTHS = useMemo(
+    () => [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Agu",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+    []
+  );
+
+
+export default function Home() {
   return (
     <div className="home">
       <FeaturedInfo />
