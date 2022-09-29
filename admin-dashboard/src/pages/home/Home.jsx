ccb095b78e9cb3591e327eba42beade145d0d08b
edielam <edguard738@gmail.com>
2022-09-29 16:46:41 +0000

    admin

--- admin-dashboard/src/pages/home/Home.jsx
+++ admin-dashboard/src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import Chart from "../../components/chart/Chart";
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
 import "./home.css";
-//import { userData } from "../../dummyData";
+import { userData } from "../../dummyData";
 import WidgetSm from "../../components/widgetSm/WidgetSm";
 import WidgetLg from "../../components/widgetLg/WidgetLg";
 import {useMemo, useState, useEffect} from 'react';
