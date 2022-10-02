d87e2aa852d9b958dce89f28d6423febfb603b89
edielam <edguard738@gmail.com>
2022-10-02 22:48:47 +0000

    admin actions

--- admin-dashboard/src/pages/home/Home.jsx
+++ admin-dashboard/src/pages/home/Home.jsx
@@ -4,8 +4,8 @@ import "./home.css";
 import { userData } from "../../dummyData";
 import WidgetSm from "../../components/widgetSm/WidgetSm";
 import WidgetLg from "../../components/widgetLg/WidgetLg";
-import {useMemo, useState, useEffect} from 'react';
-import axios from 'axios';
+import { useEffect, useMemo, useState } from "react";
+import axios from "axios";
 
 export default function Home() {
   const MONTHS = useMemo(
@@ -25,7 +25,9 @@ export default function Home() {
     ],
     []
   );
+
   const [userStats, setUserStats] = useState([]);
+
   useEffect(() => {
     const getStats = async () => {
       try {
@@ -41,7 +43,7 @@ export default function Home() {
         statsList.map((item) =>
           setUserStats((prev) => [
             ...prev,
-            { name: MONTHS[item._id - 1], "New User": item.total},
+            { name: MONTHS[item._id - 1], "New User": item.total },
           ])
         );
       } catch (err) {
@@ -50,14 +52,14 @@ export default function Home() {
     };
     getStats();
   }, [MONTHS]);
-  console.log(userStats);
+
   return (
     <div className="home">
       <FeaturedInfo />
-      <Chart data={userStats} title="User Analytics" grid dataKey="Active User"/>
+      <Chart data={userStats} title="User Analytics" grid dataKey="New User" />
       <div className="homeWidgets">
-        <WidgetSm/>
-        <WidgetLg/>
+        <WidgetSm />
+        <WidgetLg />
       </div>
     </div>
   );
