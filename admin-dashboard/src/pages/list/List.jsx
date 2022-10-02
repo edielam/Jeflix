5dcbb58e5d395a226cfc9403958fd586fc4e41bc
edielam <edguard738@gmail.com>
2022-10-02 22:51:34 +0000

    admin actions

--- admin-dashboard/src/pages/list/List.jsx
+++ admin-dashboard/src/pages/list/List.jsx
@@ -1,31 +1,54 @@
-import "./newProduct.css";
+import { Link, useLocation } from "react-router-dom";
+import "./list.css";
+import { Publish } from "@material-ui/icons";
 
-export default function NewProduct() {
+export default function List() {
+  const location = useLocation();
+  const list = location.list;
   return (
-    <div className="newProduct">
-      <h1 className="addProductTitle">New Product</h1>
-      <form className="addProductForm">
-        <div className="addProductItem">
-          <label>Image</label>
-          <input type="file" id="file" />
+    <div className="product">
+      <div className="productTitleContainer">
+        <h1 className="productTitle">List</h1>
+        <Link to="/newList">
+          <button className="productAddButton">Create</button>
+        </Link>
+      </div>
+      <div className="productTop">
+        <div className="productTopRight">
+          <div className="productInfoTop">
+            <span className="productName">{list.title}</span>
+          </div>
+          <div className="productInfoBottom">
+            <div className="productInfoItem">
+              <span className="productInfoKey">id:</span>
+              <span className="productInfoValue">{list._id}</span>
+            </div>
+            <div className="productInfoItem">
+              <span className="productInfoKey">genre:</span>
+              <span className="productInfoValue">{list.genre}</span>
+            </div>
+            <div className="productInfoItem">
+              <span className="productInfoKey">type:</span>
+              <span className="productInfoValue">{list.type}</span>
+            </div>
+          </div>
         </div>
-        <div className="addProductItem">
-          <label>Name</label>
-          <input type="text" placeholder="Apple Airpods" />
-        </div>
-        <div className="addProductItem">
-          <label>Stock</label>
-          <input type="text" placeholder="123" />
-        </div>
-        <div className="addProductItem">
-          <label>Active</label>
-          <select name="active" id="active">
-            <option value="yes">Yes</option>
-            <option value="no">No</option>
-          </select>
-        </div>
-        <button className="addProductButton">Create</button>
-      </form>
+      </div>
+      <div className="productBottom">
+        <form className="productForm">
+          <div className="productFormLeft">
+            <label>List Title</label>
+            <input type="text" placeholder={list.title} />
+            <label>Type</label>
+            <input type="text" placeholder={list.type} />
+            <label>Genre</label>
+            <input type="text" placeholder={list.genre} />
+          </div>
+          <div className="productFormRight">
+            <button className="productButton">Update</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
